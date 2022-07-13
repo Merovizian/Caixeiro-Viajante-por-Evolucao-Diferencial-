9a14c0ca6c318fe58ff5bc49cf66d1817d8aa1ee
Merovizian <ericgmicaela@gmail.com>
2022-07-13 11:50:50 -0300

    V 3.0 - PERFEITO

--- CaixeiroViajanteED.py
+++ CaixeiroViajanteED.py
@@ -1,8 +1,9 @@
 import random
 import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
 
-# PROGRAMA QUE CRIA A MATRIZ DE DISTANCIA ENTRE AS CIDADES:
-def escalaCidades(ordem, menor, maior):
+def escalaCidades(ordem, menor=1, maior=9):
     '''
     FUNCAO QUE CRIA UMA MATRIZ DE DISTANCIA ENTRE AS CIDASDES
     :param ordem: tamanho da matriz, ou seja, quantidade de cidade
@@ -17,8 +18,25 @@ def escalaCidades(ordem, menor, maior):
                 escala[a][b] = random.randint(menor, maior)
     return escala
 
-#Programa que gera a primeira população
-def populacao(tamanhoPopulacao,ordem):
+def fitDistancia(matrizElemento, matrizCidades):
+    """
+   Função que faz o calculo da distancia percorrida por cada um dos elementos
+   :param matrizCidades: No momento, não é util essa função
+   :param matrizElementos: é a matriz onde estão elencados os elementos(individuos, população)
+   :return: uma matriz com o resultado desse calculo
+   """
+    matrizResultadoDistancia = np.zeros((len(matrizElemento), 1))
+    for count, value in enumerate(matrizElemento):
+        for countA, valueA in enumerate(matrizElemento[count]):
+            try:
+                valorA = int(valueA)
+                valorB = int(matrizElemento[count][countA + 1])
+                matrizResultadoDistancia[count] += matrizCidades[valorA][valorB]
+            except:
+                pass
+    return matrizResultadoDistancia
+
+def populacao(tamanhoPopulacao,viagens ,ordem):
     '''
     Inicia a população de individuos, de acordo com a quantidade de cidades existente.
     Cada individuo carrega consigo caracteristicas, que são as cidades por onde esse individuo irá passar
@@ -27,27 +45,33 @@ def populacao(tamanhoPopulacao,ordem):
     tem que ser igual a quantidade de cidades, sem repetir
     :return:
     '''
-    elementos = list
-    povoacao = np.zeros((tamanhoPopulacao, ordem + 1))
+    povoacao = np.zeros((tamanhoPopulacao, viagens + 1))
     for a in range(tamanhoPopulacao):
-        elementos = random.sample(range(0, ordem), ordem)
+        elementos = random.sample(range(0, ordem), viagens)
         elementos.append(elementos[0])
         povoacao[a] = elementos
 
     return povoacao
 
-def fitDistancia(matrizElementos):
-    """
-   Função que faz o calculo da distancia percorrida por cada um dos elementos
-   :param matrizCidades: No momento, não é util essa função
-   :param matrizElementos: é a matriz onde estão elencados os elementos(individuos, população)
-   :return: uma matriz com o resultado desse calculo
-   """
-    matrizResultadoDistancia = np.zeros((len(matrizElementos), 1))
-    for elemento in range(len(matrizElementos)):
-        for caracteristica in range(len(matrizElementos[elemento])):
-            matrizResultadoDistancia[elemento] += matrizElementos[elemento][caracteristica]
-    return sum(matrizResultadoDistancia)
+def mutacao(matrizElementos, mutacaoTaxa):
+    '''
+    Função que faz a "mutação" de caracteristicas de cada individuo, a quantidade de caracteristicas é definido pela taxa
+    :param matrizElementos: Matriz que possui a população
+    :param mutacaoTaxa: é a taxa de caracteristicas que serão mutacionadas
+    :return: uma nova matriz com a nova geração
+    '''
+    valorMaximo = (max([valor for linha in matrizElementos for valor in linha]))
+    qntRandom = round(len(matrizElementos[1]) * mutacaoTaxa)
+    matrizGeracaoNova = matrizElementos.copy()
+    for elementos in range(len(matrizElementos)):
+        for caracteristica in range(len(matrizElementos[elementos])):
+            if caracteristica < qntRandom:
+                ValorCaracteristicaRandom = random.randint(0, valorMaximo)
+                PosicaoCaracteristicaRandom = random.randint(0, len(matrizElementos[1]) - 1)
+                while not(ValorCaracteristicaRandom not in matrizGeracaoNova[elementos]):
+                    ValorCaracteristicaRandom = random.randint(0, valorMaximo)
+                matrizGeracaoNova[elementos][PosicaoCaracteristicaRandom] = ValorCaracteristicaRandom
+    return matrizGeracaoNova
 
 def cruzamento(matrizElementos, cruzamentoTaxa):
     '''
@@ -58,74 +82,88 @@ def cruzamento(matrizElementos, cruzamentoTaxa):
     :param cruzamentoTaxa: é a porcentagem de caracteristicas que serão cruzadas
     :return: retorna uma matriz com novos elementos cruzados
     '''
-    matrizGeracaoNova = np.zeros((len(matrizElementos), len(matrizElementos[1])))
+    matrizGeracaoNova = matrizElementos.copy()
+    #matrizGeracaoNova = np.zeros((len(matrizElementos), len(matrizElementos[1])))
     for elementos in range(len(matrizElementos)):
         for caracteristica in range(len(matrizElementos[elementos])):
             if caracteristica < cruzamentoTaxa * len(matrizElementos[elementos]):
-                matrizGeracaoNova[elementos][caracteristica] = matrizElementos[elementos][caracteristica]
+                if (int(matrizElementos[elementos][caracteristica]) not in (matrizGeracaoNova[elementos])):
+                    matrizGeracaoNova[elementos][caracteristica] = matrizElementos[elementos][caracteristica]
             else:
                 try:
-                    matrizGeracaoNova[elementos][caracteristica] = matrizElementos[elementos + 1][caracteristica]
+                    if (int(matrizElementos[elementos + 1][caracteristica]) not in (matrizGeracaoNova[elementos])):
+                        matrizGeracaoNova[elementos][caracteristica] = matrizElementos[elementos + 1][caracteristica]
                 except:
-                    matrizGeracaoNova[elementos][caracteristica] = matrizElementos[0][caracteristica]
+                    if (int(matrizElementos[0][caracteristica]) not in matrizGeracaoNova[elementos]):
+                        matrizGeracaoNova[elementos][caracteristica] = matrizElementos[0][caracteristica]
 
     return matrizGeracaoNova
 
-def mutacao(matrizElementos, mutacaoTaxa):
-    '''
-    Função que faz a "mutação" de caracteristicas de cada individuo, a quantidade de caracteristicas é definido pela taxa
-    :param matrizElementos: Matriz que possui a população
-    :param mutacaoTaxa: é a taxa de caracteristicas que serão mutacionadas
-    :return: uma nova matriz com a nova geração
-    '''
-    qntRandom = round(len(matrizElementos[1]) * mutacaoTaxa)
-    matrizGeracaoNova = matrizElementos.copy()
-    for elementos in range(len(matrizElementos)):
-        for caracteristica in range(len(matrizElementos[elementos])):
-            if caracteristica < qntRandom:
-                ValorCaracteristicaRandom = random.randint(0, 9)
-                PosicaoCaracteristicaRandom = random.randint(0, len(matrizElementos[1]) - 1)
-                matrizGeracaoNova[elementos][PosicaoCaracteristicaRandom] = ValorCaracteristicaRandom
-    return matrizGeracaoNova
-
-def aplicacao(geracoes, matrizElementos):
+def aplicacao(geracoes, matrizElementos, matrizCidade):
     '''
     É o programa principal, que faz a passagem das gerações
     :param geracoes:
     :param matrizElementos:
     :return:
     '''
+    matrizFit = list()
     for linhagem in range(geracoes):
         novosElementos = matrizElementos.copy()
-        print(fitDistancia(novosElementos))
+        print(sum(fitDistancia(novosElementos,matrizCidade))/len(matrizElementos))
+        matrizFit.append(int(sum(fitDistancia(novosElementos,matrizCidade))))
         print(f"Geração: {linhagem}")
         novosElementos = cruzamento(novosElementos, cruzamentoTaxa)
         novosElementos = mutacao(novosElementos, mutacaoTaxa)
-        if fitDistancia(novosElementos) < fitDistancia(matrizElementos):
+        if sum(fitDistancia(novosElementos, matrizCidade)) < sum(fitDistancia(matrizElementos,matrizCidade)):
             matrizElementos = novosElementos.copy()
         else:
             matrizElementos = matrizElementos.copy()
 
-    return matrizElementos
-
+    return matrizElementos, matrizFit
 
 # Parametros iniciais
-quantidadeCidades = 3  # Para criar a matriz de cidades
-pessoas = 5  # População
-# viagens = 5  # Caracteristicas
-geracoes = 50000
+quantidadeCidades = 10  # Para criar a matriz de cidades
+pessoas = 50  # População
+viagens = 5  # Caracteristicas
+geracoes = 100
+listaGerações = list(range(geracoes))
 
 # Parametors para criação de novos individuos
-cruzamentoTaxa = 0.5
-mutacaoTaxa = 0.5
+cruzamentoTaxa = 0.3
+mutacaoTaxa = 0.1
 
 
-elementos = populacao(10,10)
+#APLICACAO DO PROGRAMA
+matrizCidades = escalaCidades(quantidadeCidades)
+elementos = populacao(pessoas, viagens, quantidadeCidades)
+novos = cruzamento(elementos,cruzamentoTaxa)
+resultado, matrizfit = aplicacao(geracoes,elementos,matrizCidades)
+
+
+
+'''
+plt.axis([-10, 60, 0, 11])
+plt.title("Mais incrementado")
+
+plt.grid(True)
+plt.xlabel("eixo horizontal")
+plt.ylabel("que legal")
+plt.show()'''
+
+print("***MATRIZ PRINCIPAL DAS ROTAS***")
+print(matrizCidades)
+print("***** PRIMEIRA GERAÇÃO ******")
 print(elementos)
+print("*** DISTANCIA PERCORRRIDA POR CADA ELEMENTO INICIAL ***")
+print(sum(fitDistancia(elementos, matrizCidades)))
+print("***** ULTIMA GERAÇÃO ******")
+print(resultado)
+print("*** DISTANCIA PERCORRRIDA POR CADA ELEMENTO FINAL  ***")
+print(sum(fitDistancia(resultado, matrizCidades)))
 
-
-
-
-#matrizCidades = escalaCidades(quantidadeCidades)
-#matrizCidades = [[0, 3, 5], [2, 0, 8], [1, 4, 0]]
-#print(escalaCidades(5,2,5))
+plt.plot( matrizfit )
+plt.title("Caixeiro Viajante por Evolucao Diferencial")
+plt.grid(True)
+plt.xlabel("GERAÇÕES")
+plt.ylabel("SOMA DAS DISTANCIAS")
+plt.show()
--- teste.py
+++ teste.py
@@ -1,12 +1,29 @@
 from random import sample
 import random
 import numpy as np
-ordem = 3
-elementos = list
-povoacao = escala = np.zeros((10, ordem+1))
-for a in range(10):
-    elementos= random.sample(range(0, ordem), ordem)
+tamanhoPopulacao = 10
+matrizCidades = [[0, 3, 5], [2, 0, 8], [1, 4, 0]]
+matrizElementos = [[0, 2, 1, 0], [2, 1, 0, 2], [1, 2, 0, 1]]
+'''ordem = 3
+steps = 3
+povoacao = np.zeros((tamanhoPopulacao, steps + 1))
+for a in range(tamanhoPopulacao):
+    elementos = random.sample(range(0, ordem), steps)
     elementos.append(elementos[0])
     povoacao[a] = elementos
 
 print(povoacao)
+'''
+matrizGeracaoNova = matrizElementos
+ValorCaracteristicaRandom = random.randint(0, 5)
+
+for elementos in range(len(matrizElementos)):
+    ValorCaracteristicaRandom = random.randint(0, 5)
+    while ValorCaracteristicaRandom not in matrizElementos[elementos]:
+        print(ValorCaracteristicaRandom)
+        ValorCaracteristicaRandom = random.randint(0, 5)
+        PosicaoCaracteristicaRandom = random.randint(0, len(matrizElementos[1]) - 1)
+        matrizGeracaoNova[elementos][PosicaoCaracteristicaRandom] = ValorCaracteristicaRandom
+
+print(matrizElementos)
+print(matrizGeracaoNova)
